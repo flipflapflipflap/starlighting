ed7acd1796ff5fd8ac773f46714f86c987f135d3
flipflapflipflap <112861302+flipflapflipflap@users.noreply.github.com>
2022-10-12 01:50:32 -0400

    Update rs.js

--- rs.js
+++ rs.js
@@ -26,7 +26,7 @@ var adPercent = 0.1;
 
 var Favicon_URL = `${SCRIPT_FOLDER_URL}/Images/uex.png`;
 
-var ChannelName_Caption = "Let's Flip Flapping!";
+var ChannelName_Caption = "wakarimasu";
 
 var TitleBarDescription_Caption = '>Streaming:';
 
