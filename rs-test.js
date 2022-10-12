c9920e94e9504d1e3e17caabba8f696a87202415
flipflapflipflap <112861302+flipflapflipflap@users.noreply.github.com>
2022-10-12 01:59:12 -0400

    Update rs-test.js

--- rs-test.js
+++ rs-test.js
@@ -26,7 +26,7 @@ var adPercent = 0.1;
 
 var Favicon_URL = `${SCRIPT_FOLDER_URL}/Images/kirin.png`;
 
-var ChannelName_Caption = "Wakarimasu.";
+var ChannelName_Caption = "Wakarimasu";
 
 var TitleBarDescription_Caption = '>Streaming:';
 
