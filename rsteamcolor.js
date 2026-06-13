f892559ff2116ced9a8ff165b3e33ccb809d226e
flipflapflipflap <112861302+flipflapflipflap@users.noreply.github.com>
2026-06-13 04:29:08 -0400

    Update rsteamcolor.js

--- rsteamcolor.js
+++ rsteamcolor.js
@@ -1,4 +1,5 @@
-var teamList_4cc = ["karen","hikari","mahiru","claudine","maya","junna","nana","futaba","kaoruko","mrwhite","suzdalcat","judy"];
+#var teamList_4cc = ["karen","hikari","mahiru","claudine","maya","junna","nana","futaba","kaoruko","mrwhite","suzdalcat","judy"];
+var teamList_4cc = ["karen","hikari","mahiru","claudine","maya","junna","nana","futaba","kaoruko","mrwhite","suzdalcat"];
 
 
 function setTeamList(listOfTeams){
